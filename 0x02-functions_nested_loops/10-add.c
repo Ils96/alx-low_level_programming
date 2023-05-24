b492e78a9ddf0c9b3e0882c9ba86d7a25bc9dc70
Ilias Boulaid <iliasboulaid1@gmail.com>
2023-05-23 18:56:15 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - addition
+ *@i: int to be added to l
+ *@l: int added to i
+ *Return: returning the result of the addition
+*/
+int add(int i, int l)
+{
+	int c = i + l;
+
+	return (c);
+}
