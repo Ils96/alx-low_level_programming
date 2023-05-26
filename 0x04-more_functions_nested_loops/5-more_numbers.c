88e808690cbc72a4dd39a400653e3e282f12c95a
Ilias Boulaid <iliasboulaid1@gmail.com>
2023-05-25 17:23:47 -0700

    100 time

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+*/
+void more_numbers(void)
+{
+	int i, l, z;
+	for (l = 0; l <= 10; l++)
+	{
+		for (z = 0; z <= 14; z++)
+		{
+			i = z;
+			if (z > 9)
+			{
+				_putchar(1 + 48);
+				i = z % 10;
+			}
+			_putchar(i + 48);
+		}
+		_putchar('\n');
+	}
+}
+
+
