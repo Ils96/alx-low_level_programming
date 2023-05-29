f449bf480cf59a6095c8ea303ea2ad3551fdc303
Ilias Boulaid <iliasboulaid1@gmail.com>
2023-05-29 15:46:09 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *reset_to_98 - take pointer to int parametr
+ *	and update the value to 98
+ *@n: int parameter
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This alx pointers, arrays and strings project
