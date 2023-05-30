6a43eba666ee254ad0227b9599317d36cffd4a89
Ilias Boulaid <iliasboulaid1@gmail.com>
2023-05-29 18:24:16 -0700

    second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: input string
+ */
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int start_index;
+
+	if (length % 2 == 0)
+	{
+		start_index = (length - 1) / 2;
+	}
+	while (str[start_index] != '\0')
+	{
+		_putchar(str[start_index]);
+		start_index++;
+	}
+	_putchar('\n');
+}
